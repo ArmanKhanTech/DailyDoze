2d09c6fb1d40f3d68267e0e2d8f6986d67e80272
arman <ak2341776@gmail.com>
2024-01-10 19:59:28 +0530

    Final Commit

--- app/src/main/java/com/android/dailydoze/Service/TimerService.java
+++ app/src/main/java/com/android/dailydoze/Service/TimerService.java
@@ -1,6 +1,5 @@
 package com.android.dailydoze.Service;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
